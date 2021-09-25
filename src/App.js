f5fde9defc98653fe46b881ab756dd6c8a129d00
Work <divyaakala24@gmail.com>
2021-09-25 11:48:43 +0530

    App set up, added login and reset passwotd form

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Entry from './Pages/Entry/Entry.page';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Entry />
+    </div>
+  );
+}
+
+export default App;
